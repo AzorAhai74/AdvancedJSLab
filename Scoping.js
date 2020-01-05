b5e0e9e4e979324b12102e2f05855a12f6a62c51
byron baldwin <byronsbaldwin@gmail.com>
2020-01-04 20:29:40 -0600

    nest favFruit function

--- Scoping.js
+++ Scoping.js
@@ -3,12 +3,14 @@ let fruits = ['apples', 'oranges', 'bananas'];
 function printFruit() {
     let favFruit = fruits[2];
     console.log(fruits[0]);
+
+    function printFavFruit() {
+        console.log(favFruit);
+    }
+    printFavFruit();
 }
 
-function printFavFruit() {
-    console.log(favFruit);
-}
+
 
 printFruit();
-printFavFruit();
 
